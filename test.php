eafa96755a6fc4893c871dc866a3c61a6ff7f1e6
Nader Althubaity <nader@Naders-MacBook-Pro.local>
2017-10-16 13:39:40 -0400

    First commit

---
+++ test.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: nader
+ * Date: 10/16/17
+ * Time: 1:29 PM
+ */
+
+echo "Nader is the best";
+echo "<br>Testing from IDE";
+echo " What is your favorite pet";
